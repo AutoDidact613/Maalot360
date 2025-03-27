980ddbcf3e0c85fc248d308422c014f3496e131e
פופלינגר הדסה < @m08.local>
2025-03-27 11:12:53 +0200

    Chat feature

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-
+import chatReducer from '../features/Chat/chatSlice';
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    chat: chatReducer,
+  },
 })
+
+export default store;
