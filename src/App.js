02ece4436c3a6153b998e1b4778c2a76aac7136e
Nava <navaco@gmail.com>
2025-03-28 15:46:41 +0300

    שינויים מקומיים לפני משיכה

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import MeetingsPage from './features/Nava/Meetings/Meetings';
+import Meetings from './features/Nava/Meetings/Meetings';
 
 function App() {
   return (
@@ -7,6 +9,7 @@ function App() {
       {/* <h1>Maalot 360</h1> */}
 
       <h1>Maalot 360</h1>
+      <Meetings courseId="102"/>
       </div>
   );
 }
