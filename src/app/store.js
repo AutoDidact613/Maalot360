b5e8df4bd8db4ad42dce914a9b9cbe6e45085baa
Yafit <y0527606784@gmail.com>
2025-04-03 14:12:34 +0300

    Add Update

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import eventReducer from "../features/events/EventsSlice";
-
+ import  updateReducer  from "../features/uptade/UpdatesSlice";
 export const store = configureStore({
     reducer: {
       // taskSlice:taskReducer,
 
-        events: eventReducer
+        events: eventReducer,
+         updates: updateReducer
       },
     });
--- src/components/Events.js
+++ src/components/Events.js
@@ -5,70 +5,122 @@ import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import EventForm from "../features/events/EventForm";
 import { deleteEvent } from "../features/events/EventsSlice";
-
+import { deleteUpdate } from "../features/uptade/UpdatesSlice";
+import UpdateForm from "../features/uptade/UpdateForm";
+import  "../App.css";
 const localizer = momentLocalizer(moment);
 
 function Events() {
   const dispatch = useDispatch();
-  
+   const updates = useSelector((state) => state.updates);
+
   const events = useSelector((state) => state.events.filter((event) => event.visible === true));
   const [showForm, setShowForm] = useState(false);
+  const [showUpdateForm, setShowUpdateForm] = useState(false);
   const [currentDate, setCurrentDate] = useState(new Date()); 
   const [currentEvent, setCurrentEvent] = useState(null); 
+  const [currentUpdate, setCurrentUpdate] = useState(null); 
 
   const deleteE = (id) => {
     dispatch(deleteEvent({ id }));
   };
+  const deleteU = (id) => {
+    dispatch(deleteUpdate({ id }));
+  };
   
+
   const handleNavigate = (action) => {
     setCurrentDate((prevDate) => {
       const newDate = moment(prevDate).add(action === 'PREV' ? -1 : 1, 'weeks').toDate();
       return newDate;
     });
   };
-    
   const CustomEvent = ({ event }) => {
     const eventStyles = {
-      "הגשות": { background: "#87cefa" }, 
-      "מפגשים": { background: "#1e90ff" }, 
-      "זום": { background: "#6495ed" }, 
+      "הגשות": { background: "#87cefa" },
+      "מפגשים": { background: "#1e90ff" },
+      "זום": { background: "#6495ed" },
+      "update": { background: "#ffcccb" }, // סגנון מותאם לעדכונים
     };
-
-    const defaultStyle = { background: "#E0E0E0" }; 
+  
+    const defaultStyle = { background: "#E0E0E0" };
     const style = eventStyles[event.type] || defaultStyle;
-
+  
     return (
-      <div style={{ padding: "10px", borderRadius: "3px", ...style }}>
-        <span style={{ fontSize: "1em", color: "#333" }}>{event.title}</span> <br />
-        <span style={{ fontSize: "0.8em", color: "#333" }}>📄{event.eventMessage}</span> <br />
-        <button onClick={() => deleteE(event.id)}>מחיקת אירוע</button>
-        <button onClick={() => {
-          setCurrentEvent(event);
-          setShowForm(true);
-        }}>עדכון אירוע</button>
-      </div>
+  
+      <div style={{ padding: "6px", borderRadius: "1px", ...style }}>
+  <span style={{ fontSize: event.type === "update" ? "0.8em" : "0.9em", color: "#333" }}>
+    {event.title}
+    <br />
+    {event.eventMessage}
+
+  </span>
+  {/* <br /> */}
+  <span style={{ fontSize: event.type === "update" ? "0.8em" : "0.6em", color: "#333" }}>
+    {/* {event.type === "update" ? "🔄 עדכון: " : "📄 אירוע: "} */}
+    {event.updateMessage}
+  </span>
+  <br />
+  <button onClick={() => event.type === "update" ? deleteU(event.id) : deleteE(event.id)}>
+    {event.type === "update" ? "מחיקת עדכון" : "מחיקת אירוע"}
+  </button>
+  {event.type === "update" ? (
+  <button onClick={() => {
+    setCurrentUpdate(event); 
+    setShowUpdateForm(true);
+  }}>
+    עדכון
+  </button>
+) : (
+  <button onClick={() => {
+    setCurrentEvent(event); 
+    setShowForm(true);
+  }}>
+    עדכון אירוע
+  </button>
+)}
+    
+</div>
     );
   };
 
+  const combinedEvents = [
+    ...events,
+    ...updates.map(update => ({
+      ...update,
+      start: new Date(update.start), 
+      end: new Date(update.end),     
+      type: "update",                
+    })),
+  ];
+
   return (
     <div>
-      <button onClick={() => setShowForm(true)}>➕ הוספת אירוע</button>
+      <button className="add-button" onClick={() => setShowForm(true)}>➕ הוספת אירוע</button>
       {showForm && (
         <EventForm 
           onClose={() => setShowForm(false)} 
           event={currentEvent} 
         />
       )}
-
+      {/* <br /> */}
+  <button className="add-button" onClick={() => setShowUpdateForm(true)}>➕ הוספת עידכון</button>
+      {showUpdateForm && (
+        <UpdateForm 
+          onClose={() => setShowUpdateForm(false)} 
+          update={currentUpdate} 
+        />
+      )}
+  
       <div style={{ height: "500px" }}>
         <Calendar
           localizer={localizer}
-          events={events}
+          events={combinedEvents} 
           startAccessor="start"
           endAccessor="end"
           style={{ height: "120%" }}
           rtl={true}
-          components={{ event: CustomEvent }}
+          components={{ event: CustomEvent }} // השתמש ב-CustomEvent להצגת שניהם
           date={currentDate} 
           onNavigate={(newDate) => setCurrentDate(newDate)} 
         />
