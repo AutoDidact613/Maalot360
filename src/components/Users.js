34765c433c569cf3540e595d71b9ce19e1edad87
malki <malkibiton234@gmail.com>
2025-04-22 20:33:45 +0300

    add users feature file

---
+++ src/components/Users.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Users = () => {
+  return (
+    <div>
+      <h1>Users</h1>
+      <p>List of users and their details...</p>
+    </div>
+  );
+};
+
+export default Users;
