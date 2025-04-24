971090f2da8ea6c24e7e3f93fe95c957bb23f809
grinsh <sh0504128171@gmail.com>
2025-04-24 11:23:06 +0300

    init students feature

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import studentSlice from "../features/students/studentSlice";
 
-
-import { configureStore } from '@reduxjs/toolkit'
-import chatReducer from '../features/Chat/chatSlice';
-import taskReducer from '../featuers/lecturer/taskSlice'
-import tasksReducer from '../features/tasks/tasksSlice'; 
 export const store = configureStore({
-  reducer: {
-     tasks: tasksReducer,
-     taskSlice:taskReducer,
-     chat: chatReducer
-  },
-})
-
+    reducer: {
+        users: studentSlice,
+    }
+});
--- src/index.js
+++ src/index.js
@@ -3,30 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store } from './app/store';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { SelectCours } from './featuers/lecturer/SelectCours';
-import TaskList from './featuers/lecturer/TaskList';
-import { AddTask } from './featuers/lecturer/AddTask';
-import { AddQuestions } from './featuers/lecturer/AddQuestions';
+import { store } from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <h1>Hello</h1>
-                <Routes>
-                <Route path="/add-questions/:taskName/course/:courseName" element={<AddQuestions />} />
-                    <Route path="/" element={<SelectCours />} />
-                    <Route path="/add-task/:courseName" element={<AddTask />} />
-                    <Route path="/TaskList/:courseName" element={<TaskList />} />
-                    <Route path="*" element={<App />} /> 
-                </Routes>
-            </BrowserRouter>
-        </Provider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider  store={store} >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
