f3d968942b5ef067d0b864b0a2e6e2f35e8a56b2
grinsh <sh0504128171@gmail.com>
2025-04-24 10:07:37 +0300

    full homecontact feature

--- src/app/store.js
+++ src/app/store.js
@@ -4,11 +4,15 @@ import { configureStore } from '@reduxjs/toolkit'
 import chatReducer from '../features/Chat/chatSlice';
 import taskReducer from '../featuers/lecturer/taskSlice'
 import tasksReducer from '../features/tasks/tasksSlice'; 
+import registrationReducer from '../features/registration/registrationSlice';
+
 export const store = configureStore({
   reducer: {
      tasks: tasksReducer,
      taskSlice:taskReducer,
-     chat: chatReducer
+     chat: chatReducer,
+     registration: registrationReducer, 
+
   },
 })
 
