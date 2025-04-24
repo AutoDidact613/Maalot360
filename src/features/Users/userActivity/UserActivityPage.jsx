0a1cdd49adedf3610f6ece1bce3fe4891ac03455
malki <malkibiton234@gmail.com>
2025-04-24 14:35:39 +0300

    Add button of activity

--- src/features/Users/userActivity/UserActivityPage.jsx
+++ src/features/Users/userActivity/UserActivityPage.jsx
@@ -1,15 +1,46 @@
-import React from 'react';
+// import React from 'react';
+// import { useSelector } from 'react-redux';
+// import ActivityItem from './ActivityItem'; // ודאי שהנתיב נכון לפי מיקום הקובץ שלך
+
+// const UserActivityPage = () => {
+//   const activities = useSelector(state => state.userActivity.activities);
+
+//   return (
+//     <div>
+//       {activities.map((activity) => (
+//         <ActivityItem key={activity.id} activity={activity} />
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default UserActivityPage;
+
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import ActivityItem from './ActivityItem'; // ודאי שהנתיב נכון לפי מיקום הקובץ שלך
+import ActivityItem from './ActivityItem';
 
 const UserActivityPage = () => {
   const activities = useSelector(state => state.userActivity.activities);
+  const [showActivities, setShowActivities] = useState(false); // ניהול מצב הצגת הפעילויות
+
+  const toggleActivities = () => {
+    setShowActivities(prevState => !prevState); // משנה את המצב להצגת הפעילויות
+  };
 
   return (
     <div>
-      {activities.map((activity) => (
-        <ActivityItem key={activity.id} activity={activity} />
-      ))}
+      <button onClick={toggleActivities}>
+        {showActivities ? 'הסתר פעילויות' : 'הצג פעילויות'}
+      </button>
+      
+      {showActivities && (
+        <div>
+          {activities.map((activity) => (
+            <ActivityItem key={activity.id} activity={activity} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
