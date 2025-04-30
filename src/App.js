22ba5d6490402d303148906afcf9d90510e1b6d6
malki <malkibiton234@gmail.com>
2025-05-01 01:08:02 +0300

    Update app and create a profile butoon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 
+
+import { useState } from 'react'; 
 import TaskList from './featuers/lecturer/TaskList';
 import { SelectCours } from './featuers/lecturer/SelectCours';
 import EditProfileModal from './features/Users/EditProfileModal';
@@ -7,25 +9,35 @@ import ProfilePage from './features/Users/ProfilePage';
 import ActivityChart from './features/Users/userActivity/ActivityChart';
 import UserActivityPage from './features/Users/userActivity/UserActivityPage';
 import { useSelector } from 'react-redux';
+import InactiveUsersCards from './features/Users/InactiveUsersCards';
+import ProfileMenuWrapper from './features/Users/ProfileMenuWrapper';
 
 function App() {
   const currentUser = useSelector((state) => state.users.currentUser);
+  const [showInactiveUsers, setShowInactiveUsers] = useState(false);
 
   return (
     <div className="App">
       <h1>Maalot 360</h1>
-       <TaskList />
+      <TaskList />
       <SelectCours />
       {!currentUser ? (
         <LogIn />
       ) : (
         <>
-          <button onClick={() => console.log("היסטוריית פעילות")}>
-            היסטוריית פעילות
+              {currentUser ? <ProfileMenuWrapper /> : <LogIn />}
+
+          <button onClick={() => setShowInactiveUsers(!showInactiveUsers)}>
+            {showInactiveUsers ? 'חזרה לפרופיל' : 'הצג משתמשים לא פעילים'}
           </button>
-          <ActivityChart userId={currentUser.id} />
-          <UserActivityPage userId={currentUser.id} />
-          <ProfilePage currentUser={currentUser} />
+
+          {showInactiveUsers ? (
+            <InactiveUsersCards />
+          ) : (
+            <>
+              
+            </>
+          )}
         </>
       )}
     </div>
@@ -33,4 +45,3 @@ function App() {
 }
 
 export default App;
-
