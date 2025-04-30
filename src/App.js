40fd155b2f23b30300f10ffb100cf94b0fb5d2b0
grinsh <shoshigrin18@gmail.com>
2025-04-30 21:39:50 +0300

    clean main branch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
-import  TaskList  from './featuers/lecturer/TaskList';
-import { SelectCours } from './featuers/lecturer/SelectCours';
+
 
 function App() {
 
@@ -8,12 +7,8 @@ function App() {
 
   return (
     <div className="App">
-      {/* <h1>Maalot 360</h1> */}
-
-<h1>fghjk</h1>
       <h1>Maalot 360</h1>
-      <TaskList/>
-        <SelectCours />
+    
       </div>
   );
 }
