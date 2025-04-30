471d53e57789134c3f16f3f255993c70220cc94f
hadas-dayan <hadasda8@gmail.com>
2025-04-30 18:30:15 +0300

    שינויים ב־attendanceSlice.js

--- src/features/Attendance/attendanceSlice.js
+++ src/features/Attendance/attendanceSlice.js
@@ -1,21 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  courses: [
-    { name: 'HTML ', sessions: 12 },
-    { name: 'JAVA', sessions: 14 },
-    { name: 'UI UX', sessions: 10 },
-  ],
+    courses: [
+        { name: 'HTML ', sessions: 12 },
+        { name: 'JAVA', sessions: 14 },
+        { name: 'UI UX', sessions: 10 },
+    ],
 };
 
 const viewAllAttendanceSlice = createSlice({
-  name: 'viewAllAttendance',
-  initialState,
-  reducers: {
-    setCourses: (state, action) => {
-      state.courses = action.payload;
+    name: 'viewAllAttendance',
+    initialState,
+    reducers: {
+        setCourses: (state, action) => {
+            state.courses = action.payload;
+        },
     },
-  },
 });
 
 export const { setCourses } = viewAllAttendanceSlice.actions;
