9e988ef40f078c8233499377477bcfee1a88b459
Hadasa Tzipy <h0504130396@g mail.com>
2025-04-30 21:06:06 +0300

    clean conflicts2

--- src/app/store.js
+++ src/app/store.js
@@ -1,39 +1,11 @@
 
 
-// import { configureStore } from '@reduxjs/toolkit'
-// import chatReducer from '../features/Chat/chatSlice';
-// import taskReducer from '../featuers/lecturer/taskSlice'
-// import tasksReducer from '../features/tasks/tasksSlice'; 
-
-// export const store = configureStore({
-//   reducer: {
-//      tasks: tasksReducer,
-//      taskSlice:taskReducer,
-//      Chatt: chatReducer
-//   },
-// })
-
 import { configureStore } from '@reduxjs/toolkit';
 import chatReducer from '../features/Chat/chatSlice';
-<<<<<<< HEAD
 
 export const store = configureStore({
   reducer: {
     Chatt: chatReducer,
-=======
-import taskReducer from '../featuers/lecturer/taskSlice'
-import tasksReducer from '../features/tasks/tasksSlice'; 
-import registrationReducer from '../features/registration/registrationSlice';
-import chatReducer from '../features/Chat/chatSlice';
-
-export const store = configureStore({
-  reducer: {
-     tasks: tasksReducer,
-     taskSlice:taskReducer,
-     Chatt: chatReducer,
-     registration: registrationReducer, 
-
->>>>>>> 56a994e0fe7b48852ff3fde4a17ce7638cc57d97
   },
 });
 
