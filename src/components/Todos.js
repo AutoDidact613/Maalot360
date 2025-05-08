06058ed3d8be40013c98869d036313df35c7417c
shoshi tepp <shoshi@grin.com>
2025-05-08 10:08:41 +0300

    add fiture to component

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import AddTask from '../features/tasks/AddTask';
+import EditTaskForm from '../features/tasks/EditTaskForm';
+import FilterBar from '../features/tasks/FilterBar';
+import TaskList from '../features/tasks/TaskList';
 
 const Tasks = () => {
   return (
     <div>
-      <h1>Tasks</h1>
-      <p>List of tasks and their details...</p>
+      <h1>Todos</h1>
+        <AddTask />
+        <FilterBar />
+        <TaskList />
     </div>
   );
 };
